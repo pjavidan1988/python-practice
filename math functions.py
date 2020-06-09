faf5f98ac62893d9910ba811fad294cb374d821b
Peyman Javidan <p.javidan1988@gmail.com>
2020-06-09 23:20:08 +0430

    math functions

---
+++ math functions.py
@@ -0,0 +1,4 @@
+# math functions
+# print(round(3.9))  # rounding the number to the side that is closer
+# print(abs(-20))  # return the absolute value of the argument(Don't be negative)
+# (search google for other math function)
