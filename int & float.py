f2f5c0d6391bbd5bea214840af2d131f598af5c1
Peyman Javidan <p.javidan1988@gmail.com>
2020-06-09 23:18:19 +0430

    int & float

---
+++ int & float.py
@@ -0,0 +1,11 @@
+# Numbers :
+# int (signed integers) : 10, 2, -10, ...
+# print(type(10))
+# float (floating point real values) : 0.0, 15.20, -21.9, ...
+# print(type(0.0))
+# complex (complex numbers) : 3.14j,  45.j, 9.322e-36j, ...
+# print(type(3.14j))
+#---------- tip
+# print(2 ** 3)  # power
+# print(80 // 52)  # rounded down integer
+# print(6 % 4)  # remainder of this division
