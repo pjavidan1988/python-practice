9617b47f35282aadfb8d3c7d4446549cf9d4fd4a
Peyman Javidan <p.javidan1988@gmail.com>
2020-06-10 23:41:44 +0430

    variable

---
+++ Variables.py
@@ -0,0 +1,22 @@
+#Variables are nothing but reserved memory
+# locations to store values.
+# It means that when you create a variable,
+# you reserve some space in the memory.
+# The equal sign (=) is used to assign values to variables.
+# The operand to the left of the = operator is the
+# name of the variable and the operand to the right
+# of the = operator is the value
+# stored in the variable.
+counter = 100
+print(counter)
+
+# snake_case -> user_iq = 190
+# start with lowercase or underscore
+# letters, numbers, underscore
+# case sensitive
+# don't overwrite keywords -> print = 190 -> the mistake
+
+a, b, c = 1, 2, 3
+print(a)
+print(b)
+print(c)
