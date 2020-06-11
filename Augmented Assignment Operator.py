016bb45926ab9e789e656fab425f1205a1675515
Peyman Javidan <p.javidan1988@gmail.com>
2020-06-11 10:09:25 +0430

    Augmented Assignment Operator

---
+++ Augmented Assignment Operator.py
@@ -0,0 +1,26 @@
+# Augmented Assignment Operator
+# some_value = 5
+# some_value = some_value + 5
+#
+#
+# print(some_value)
+
+
+# some_value = 5
+# some_value += 5
+#
+#
+# print(some_value)
+
+
+# some_value = 5
+# some_value -= 5
+#
+#
+# print(some_value)
+
+
+# some_value = 5
+# some_value *= 5
+#
+# print(some_value)
