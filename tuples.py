0dc8d13983870a02d5e8a4ad42db033af3670abc
peyman javidan <p.javidan1988@gmail.com>
2020-06-18 13:20:03 +0430

    tuple

---
+++ tuples.py
@@ -0,0 +1,25 @@
+# tuple :
+# my_tuple = (1, 2, 3, 4, 5)
+# print(5 in my_tuple)  # -> True
+
+# new_tuple = my_tuple[1:2]
+# print(new_tuple)  # -> (2,)
+
+# new_tuple = my_tuple[1:4]
+# print(new_tuple)  # -> (2, 3, 4)
+
+# x, y, z, *other = (1, 2, 3, 4, 5)
+#
+# print(x)  # -> 1
+# print(y)  # -> 2
+# print(other)  # -> [4, 5]
+
+# my_tuple = (1, 2, 3, 4, 5, 5)
+# print(my_tuple.count(5))  # -> 2
+# print(my_tuple.count(1))  # -> 1
+# print(my_tuple.index(3))  # -> 2
+# print(my_tuple.index(5))  # -> 5
+# print(len(my_tuple))  # -> 6
+
+
+
